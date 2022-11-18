cdafbb49871414fd2a5f911a6009c799d3de6fc3
will <jaehwang.lee@bucketplace.net>
2022-11-18 15:54:29 +0900

    init project
    update readme

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import * as core from '@actions/core'
+import {getMilestoneIssues} from './github'
+import {sendToSlack} from './slack'
+
+async function run(): Promise<void> {
+  try {
+    const version: string = core.getInput('version', {required: true})
+    const slackReceiverUser: string = core.getInput('slack-receiver-user', {
+      required: false
+    })
+    const slackReceiverTeam: string = core.getInput('slack-receiver-team', {
+      required: false
+    })
+
+    core.debug(`Deploy Notification To Slack version: ${version}`)
+
+    const milestoneIssues = await getMilestoneIssues(version)
+    await sendToSlack(
+      milestoneIssues,
+      version,
+      slackReceiverUser,
+      slackReceiverTeam
+    )
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+run()
